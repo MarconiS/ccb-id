90ea062899214b3cb5980563c99c11fa9c4664be
MarconiS <sergio.marconi@weecology.org>
2019-02-09 11:25:54 -0500

    Update scale.py

--- scale.py
+++ scale.py
@@ -7,10 +7,10 @@ import ccbid
 from ccbid import args
 from ccbid import prnt
 
-pt = "/Users/sergiomarconi/"
+pt = "/orange/ewhite/NeonData/2015_Campaign/D03/OSBS/L5/Rasters/"
 ainput = "NIS1_20140507_144927_atmcor_CNJRF.tif"
-aitcput = "/Users/sergiomarconi/"+ainput[:-4]+".shp"
-#ainput = args[1]
+aitcput = "/orange/ewhite/NeonData/2015_Campaign/D03/OSBS/L5/ITCs/"+ainput[:-4]+"_silva.shp"
+ainput = sys.argv[1]
 aremove_outliers = 'PCA'
 athreshold = 3
 aaggregate = 'average'
@@ -60,8 +60,8 @@ from rasterio import features
 
 itc = gpd.read_file(aitcput)
 
-final = final.values.reshape(npix[0], npix[1], final.shape[1])
-with rasterio.open(ainput) as dataset:
+#final = final.values.reshape(npix[0], npix[1], final.shape[1])
+with rasterio.open(pt+ainput) as dataset:
     extent = dataset.transform
     data_crs = dataset.crs
 
@@ -70,7 +70,8 @@ with rasterio.open(ainput) as dataset:
 from rasterio.transform import from_origin
 final = final.astype(float)
 
-new_dataset = rasterio.open('test1.tif', 'w', driver='GTiff',
+pt_out = "/orange/ewhite/NeonData/2015_Campaign/D03/OSBS/L5/SPAnde/"    
+new_dataset = rasterio.open(pt_out+ainput[:-4]+'_sp.tif', 'w', driver='GTiff',
                             height = final.shape[0], width = final.shape[1],
                             count=prob.shape[1], dtype=str(final.dtype),
                             crs=data_crs,
@@ -79,6 +80,8 @@ new_dataset = rasterio.open('test1.tif', 'w', driver='GTiff',
 for ii in range(prob.shape[1]):
     tmp = final[:,:,ii].astype(float)
     new_dataset.write(tmp, ii+1)
+
+
 new_dataset.close()
 
 from rasterstats import zonal_stats
@@ -91,17 +94,15 @@ itc = gpd.read_file(aitcput)
 itc.crs = data_crs
 
 for ii in range(prob.shape[1]):
-    with rasterio.open("./polygons/rasters/"+ainput) as src:
+    with rasterio.open(pt_out+ainput[:-4]+'_sp.tif') as src:
         affine = src.transform
         array = src.read(ii+1)
-        df_zonal_stats = pd.DataFrame(zonal_stats(itc, array, affine=affine, stats=['mean']))
-
-    # adding statistics back to original GeoDataFrame
-    itc = pd.concat([itc, df_zonal_stats], axis=1) 
+        df_zonal_stats = pd.DataFrame(zonal_stats(itc, array, affine=affine, stats=['mean']))   
+    itc = pd.concat([itc, df_zonal_stats], axis=1)
 
 
-
-csv_name = "./predictions/polygons/"+ainput[:-4]
+poly_out = "/orange/ewhite/NeonData/2015_Campaign/D03/OSBS/L5/Sp_poly/" 
+csv_name = poly_out+ainput[:-4]
 itc.dropna().to_csv(csv_name+".csv", index=False) 
 
 
