c4818664701d1c90b4023920953cb095aafe028e
MarconiS <sergio.marconi@weecology.org>
2019-02-09 19:00:26 -0500

    Update scale.py

--- scale.py
+++ scale.py
@@ -110,7 +110,7 @@ for ii in range(prob.shape[1]):
 
 poly_out = "/orange/ewhite/NeonData/2015_Campaign/D03/OSBS/L5/Sp_shp/" 
 csv_name = poly_out+ainput[:-4]
-itc.to_file(csv_name+".shp", )
+itc.to_file(csv_name+".shp")
 #itc.to_csv(csv_name+".csv", index=False) 
 
 
