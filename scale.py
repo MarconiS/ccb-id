e1f073f6fbb2158939f7004b77e570fde45df936
MarconiS <sergio.marconi@weecology.org>
2019-02-10 17:52:07 -0500

    Update scale.py

--- scale.py
+++ scale.py
@@ -10,9 +10,9 @@ from ccbid import prnt
 
 
 pt = "/orange/ewhite/NeonData/2015_Campaign/D03/OSBS/L5/Rasters/"
-ainput = "NIS1_20140507_143910_atmcor_BRTYP.tif"
+#ainput = "NIS1_20140507_143910_atmcor_BRTYP.tif"
 aitcput = "/orange/ewhite/NeonData/2015_Campaign/D03/OSBS/L5/ITCs/"+ainput[:-4]+"_silva.shp"
-#ainput = sys.argv[1]
+ainput = sys.argv[1]
 aremove_outliers = 'PCA'
 athreshold = 3
 aaggregate = 'average'
