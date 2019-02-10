fb8b964f0dffc9a17d85c6a8c396df1abc98803e
MarconiS <sergio.marconi@weecology.org>
2019-02-10 17:10:42 -0500

    Update scale.py

--- scale.py
+++ scale.py
@@ -7,15 +7,18 @@ import ccbid
 from ccbid import args
 from ccbid import prnt
 
+
+
 pt = "/orange/ewhite/NeonData/2015_Campaign/D03/OSBS/L5/Rasters/"
+ainput = "NIS1_20140507_143910_atmcor_BRTYP.tif"
 aitcput = "/orange/ewhite/NeonData/2015_Campaign/D03/OSBS/L5/ITCs/"+ainput[:-4]+"_silva.shp"
-ainput = sys.argv[1]
+#ainput = sys.argv[1]
 aremove_outliers = 'PCA'
 athreshold = 3
 aaggregate = 'average'
 # args.feature_selection = False
 
-
+# set the seed for reproducibility (to the year the CCB was founded)
 np.random.seed(1984)
 model = ccbid.read.pck("./model/ensemble")
 
@@ -25,17 +28,13 @@ sp_labels = model.labels_
 from skimage import io
 features = np.array(io.imread(pt+ainput))
 npix = features.shape
-features = features.flatten().reshape(npix[0]*npix[1], 369)
+features = features.flatten().reshape(npix[0]*npix[1], 369) /10000
 
-
-#use ndvi and nir masks
 ndvi = (features[:,90] - features[:, 58])/(features[:,58] +features[:,90]) <0.5
 nir860 = (features[:,96] + features[:,97])/2 < 0.4
 naval = ~(ndvi | nir860)
-print(naval.shape)
 mask1 = np.all(features < 0, axis=1) | np.all(features > 1, axis=1) | naval
 data = features[~mask1] 
-
 #np.apply_along_axis(my_func, 0, b)
 normMat = np.apply_along_axis(np.sum, 1, data**2)
 normMat = np.sqrt(normMat)
@@ -44,11 +43,10 @@ normMat = np.tile(normMat, (data.shape[1],1))
 data=data / np.transpose(normMat)
 normMat = None
 data.shape
-
-
+#normMat=df.drop(['crown_id'], axis=1) / np.transpose(normMat)
 mask_pca = ccbid.outliers.with_pca(data, thresh=athreshold)
 data = data[mask_pca, 24:369]
-#id_labels = id_labels[mask]
+
 
 data = model.reducer.transform(data)
 data = data[:, 0:model.n_features_]
@@ -56,27 +54,27 @@ prnt.status("Applying CCBID model to input features")
 prob = model.predict_proba(data, average_proba=True)
 
 output = pd.DataFrame(index=range(mask_pca.shape[0]), columns=range(prob.shape[1]))
-
 output.loc[mask_pca,:] =  prob
 final = pd.DataFrame(index=range(features.shape[0]), columns=range(prob.shape[1]))
 final[~mask1] = output
+final = final.values.reshape(npix[0], npix[1], final.shape[1])
+final = final.astype(float)
 
-#get the crown ids from the crown delinetation
+
 import geopandas as gpd
 import rasterio
 from rasterio import features
+from rasterio.transform import from_origin
+from rasterstats import zonal_stats
 
-itc = gpd.read_file(aitcput)
-
-final = final.values.reshape(npix[0], npix[1], final.shape[1])
 with rasterio.open(pt+ainput) as dataset:
     extent = dataset.transform
     data_crs = dataset.crs
 
 
-
-from rasterio.transform import from_origin
-final = final.astype(float)
+#get the crown ids from the crown delinetation
+itc = gpd.read_file(aitcput)
+itc.crs = data_crs
 
 pt_out = "/orange/ewhite/NeonData/2015_Campaign/D03/OSBS/L5/SPAnde/"    
 new_dataset = rasterio.open(pt_out+ainput[:-4]+'_sp.tif', 'w', driver='GTiff',
@@ -88,29 +86,30 @@ new_dataset = rasterio.open(pt_out+ainput[:-4]+'_sp.tif', 'w', driver='GTiff',
 for ii in range(prob.shape[1]):
     tmp = final[:,:,ii].astype(float)
     new_dataset.write(tmp, ii+1)
+
+
 new_dataset.close()
 
-from rasterstats import zonal_stats
-#get the crown ids from the crown delinetation
-import geopandas as gpd
-import rasterio
-from rasterio import features
-
-itc = gpd.read_file(aitcput)
-itc.crs = data_crs
 
 for ii in range(prob.shape[1]):
     with rasterio.open(pt_out+ainput[:-4]+'_sp.tif') as src:
         affine = src.transform
         array = src.read(ii+1)
-        df_zonal_stats = pd.DataFrame(zonal_stats(itc, array, affine=affine, stats=['mean']))   
-    itc = pd.concat([itc, df_zonal_stats], axis=1)
+        df_zonal_stats = pd.DataFrame(zonal_stats(itc, array, affine=affine, stats=['mean']))
+    # adding statistics back to original GeoDataFrame
+    itc = pd.concat([itc, df_zonal_stats], axis=1) 
 
 
-poly_out = "/orange/ewhite/NeonData/2015_Campaign/D03/OSBS/L5/Sp_shp/" 
+poly_out = "/orange/ewhite/NeonData/2015_Campaign/D03/OSBS/L5/Sp_poly/" 
 csv_name = poly_out+ainput[:-4]
-itc.to_file(csv_name+".shp")
-#itc.to_csv(csv_name+".csv", index=False) 
+#itc.to_file(csv_name+".shp")
+
+itc.to_csv(csv_name+".csv", index=False) 
+
+
+
+
+
 
 
 
